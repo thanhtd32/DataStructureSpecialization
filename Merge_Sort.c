0da84fc4f537c5b92893b51e0d795760f70aba16
thanhtd32 <53929436+thanhtd32@users.noreply.github.com>
2022-06-07 22:41:21 +0900

    Add files via upload

--- Merge_Sort.c
+++ Merge_Sort.c
@@ -10,16 +10,14 @@ Professor:Kil-Woong Jang (jangkw@kmou.ac.kr)
 #define SWAP(x, y, t) ( (t)=(x), (x)=(y), (y)=(t) )
 int sorted[MAX_SIZE]; 
 // merge method
-void merge(int list[], int left, int mid, int right)
-{
+void merge(int list[], int left, int mid, int right){
     // i is the index into the sorted left list
     // j is the index into the sorted right list
     // k is the index into the list to be sorted
     int i, j, k, l;
     i=left; j=mid+1; k=left;
     // Merge split sorted list
-    while(i<=mid && j<=right)
-    {
+    while(i<=mid && j<=right) {
         if(list[i]<=list[j]) 
             sorted[k++] = list[i++];
         else 
@@ -36,11 +34,9 @@ void merge(int list[], int left, int mid, int right)
         list[l] = sorted[l];
 }
 //merge sort method
-void merge_sort(int list[], int left, int right)
-{
+void merge_sort(int list[], int left, int right){
     int mid;
-    if(left<right)
-    {
+    if(left<right){
         mid = (left+right)/2; // evenly split the list
         merge_sort(list, left, mid); // sort partial list
         merge_sort(list, mid+1, right);//sort partial list
@@ -48,8 +44,7 @@ void merge_sort(int list[], int left, int right)
     }
 }
 //test case
-int main(void)
-{
+int main(void){
     int n = 8;
     int list[]={67, 90, 57, 25, 84, 32, 73, 54};
     //call merge sort method
